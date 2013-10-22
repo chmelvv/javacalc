abc131d1beec86f092c24c82e7891c3c56f15c31
Viktor Chmel <Viktor.Chmel@gmail.com>
2013-10-22 18:27:03 +0300

    add +-/*

--- src/com/practicaljava/lesson8/tryit/CalculatorEngine.java
+++ src/com/practicaljava/lesson8/tryit/CalculatorEngine.java
@@ -9,9 +9,10 @@ public class CalculatorEngine implements ActionListener {
 	private Double dOperand1, dOperand2;
 	private String text="";
 	char sign='+'; //Operand sign
-	Boolean itext1Entered=false;
+	Boolean isText1Entered=false;
     Boolean isPoint=false; //is "." exist in operand
-    Boolean isDivide, isMultiply, isAdd, isSubstruct, isPercent;
+    Boolean isDivide=false, isMultiply=false, isAdd=false, isSubstract=false, isPercent=false;
+    Boolean isNumberOnlyEntered=false;
      
 
 	CalculatorEngine(CalculatorGBL parent){
@@ -27,6 +28,67 @@ public class CalculatorEngine implements ActionListener {
 			parent.setDisplayValue(sign + str);
 		}
 	}
+	
+	void divide(){
+		dOperand2 = Double.parseDouble(sign+text);
+		dOperand1 = dOperand1 / dOperand2;
+		text = Double.toString(dOperand1);
+		show(text);
+	
+		dOperand2 = null;
+		isText1Entered = false;
+		isDivide=false;
+		isPoint=false;
+		sign='+';
+		text="";
+	}
+	
+	void multiply(){
+		dOperand2 = Double.parseDouble(sign+text);
+		dOperand1 = dOperand1 * dOperand2;
+		text = Double.toString(dOperand1);
+		show(text);
+	
+		dOperand2 = null;
+		isText1Entered = false;
+		isMultiply=false;
+		isPoint=false;
+		sign='+';
+		text="";
+	}
+	
+	void add(){
+		dOperand2 = Double.parseDouble(sign+text);
+		dOperand1 = dOperand1 + dOperand2;
+		text = Double.toString(dOperand1);
+		show(text);
+	
+		dOperand2 = null;
+		isAdd=false;
+		isText1Entered = false;
+		isPoint=false;
+		sign='+';
+		text="";
+	}
+
+	void substract(){
+		dOperand2 = Double.parseDouble(sign+text);
+		dOperand1 = dOperand1 - dOperand2;
+		if (dOperand1 < 0) {
+			dOperand1 = dOperand1*(-1);
+			sign='-';
+		}
+		text = Double.toString(dOperand1);
+		show(text);
+	
+		dOperand2 = null;
+		isSubstract=false;
+		isText1Entered = false;
+		isPoint=false;
+		sign='+';
+		text="";
+	}
+
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -44,8 +106,17 @@ public class CalculatorEngine implements ActionListener {
 			case "7":
 			case "8":
 			case "9":
-				text += label;
-				show(text);
+				if (!isNumberOnlyEntered) {
+					text += label;
+					show(text);
+				} else {
+					text=label;
+					isPoint=false;
+					sign='+';
+					isNumberOnlyEntered=false;
+					isText1Entered=false;
+					show(text);
+				}
 				break;
 			
 			case ".":
@@ -57,63 +128,108 @@ public class CalculatorEngine implements ActionListener {
 				break;
 			
 			case "+/-":
-				sign = (sign == '+')? '-' : '+';
+				sign = (sign == '+') ? '-' : '+';
 				show(text);
 				break;
 				
-			case "Backspace":
-			case "C":
+			case "Backspace": // delete last symbol
 				if ( !text.isEmpty() ) {
-					 text = text.substring(0, text.length()-1);
-				} else if (sign == '-') {sign='+';}
+					 text = text.substring(0, text.length()-1); // delete last symbol
+				} else if (sign == '-') {sign='+';} //in empty text - set sign to default
+				show(text);
+				break;
+				
+			case "C": // delete last operand, remain operation
+				// (+-N.N op +-M.M) OR (+-N.N op) OR (+-N.N) 
+				text="";
+				isPoint=false;
+				sign='+';			
 				show(text);
 				break;
 			
-			case "CE":
+			case "CE": //Total calc clearing
+				dOperand2 = dOperand1 = null;
+				isText1Entered = false;
+						
+				isDivide = false;
+				isMultiply = false;
+				isAdd = false;
+				isSubstract = false;
+				isPercent = false;
+
 				text="";
-				sign='+';
+				isPoint=false;
+				sign='+';			
 				show(text);
+				
 				break;
 				
 			case "/":
-				if (!itext1Entered) {
-//					dOperand1 = Double.parseDouble(sign+text);
-//					itext1Entered=true;
-//					text="";
-//					sign='+';
-//					isPoint=false;
-//					isDivide = true;
-//					show(text);
+				if (!isText1Entered) {
+					dOperand1 = Double.parseDouble(sign+text);
+					isText1Entered=true;
+
+					isDivide = true;
+					isPoint=false;
+					sign='+';
+					text="";
 				} else { // "/" <=> "="
-//					dOperand1 = dOperand1 / Double.parseDouble(sign+text);
-//					text = dOperand1.toString();
-//					dOperand2 = null;
-//					itext1Entered=true;
-//					show(text);
-				}
+					divide();				}
 				break;
+
+			case "*":
+				if (!isText1Entered) {
+					dOperand1 = Double.parseDouble(sign+text);
+					isText1Entered=true;
+
+					isMultiply = true;
+					isPoint=false;
+					sign='+';
+					text="";
+				} else { // "/" <=> "="
+					multiply();				}
+				break;
+
+			case "-":
+				if (!isText1Entered) {
+					dOperand1 = Double.parseDouble(sign+text);
+					isText1Entered=true;
+
+					isSubstract = true;
+					isPoint=false;
+					sign='+';
+					text="";
+				} else { // "/" <=> "="
+					substract();				}
+				break;
+				
+			case "+":
+				if (!isText1Entered) {
+					dOperand1 = Double.parseDouble(sign+text);
+					isText1Entered=true;
+
+					isAdd = true;
+					isPoint=false;
+					sign='+';
+					text="";
+				} else { // "/" <=> "="
+					add();				}
+				break;
+
 			case "=":
-				if (itext1Entered) {
-//					dOperand2 = Double.parseDouble(sign+text);
-//					if (isDivide) { 
-//						dOperand1 = dOperand1 / Double.parseDouble(sign+text);
-//						text = dOperand1.toString();
-//						dOperand2 = null;
-//						itext1Entered=true;
-//						isDivide=false;
-//						show(text);
-//						}
-				} else {
-					isDivide = false;
-					isMultiply = false;
-					isAdd = false;
-					isSubstruct = false;
-					isPercent = false;
+				if (isText1Entered) {// Case "N op M ="
+					dOperand2 = Double.parseDouble(sign+text);
+					if (isDivide) { divide(); } 
+					if (isMultiply) { multiply(); }
+					if (isSubstract) { substract(); }
+					if (isAdd) { add(); }
+					isText1Entered = false;
 					
+				} else { // Case "N ="
+					dOperand1 = Double.parseDouble(sign+text);
+					isText1Entered=true;
 					show(text);
-					text = "";
-					isPoint = false;
-					
+					isNumberOnlyEntered=true; // Case when typed "N=M" must be equals "M"
 				}
 				break;
 				
